32719e230c108b65b40792bdf553f606dec9902d
subhamx <subham7sahu@gmail.com>
2025-01-26 15:33:24 +0100

    refactor: Simplify navbar imports and remove unused components

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,26 +1,9 @@
 'use server'
 
-import Link from "next/link"
-import { Button } from "./ui/button"
-import { AUTH_COOKIE_NAME, getUserInServer } from "@/app/utils/setAuthTokenAsCookie"
-import { RainbowButton } from "./ui/rainbow-button"
-import { getLoginUrl } from "@/app/getAuthUrl"
-import { cookies } from "next/headers"
-import { LANDING_ROUTE } from "@/routes.config"
-import { redirect } from "next/navigation"
-import { LoginButton, LogoutButton } from "./ClientNavbarButtons"
+import { getUserInServer } from "@/app/utils/setAuthTokenAsCookie"
 import { eq } from "drizzle-orm"
 import { db } from "@/drizzle/db-config"
 import { accounts, usersToAccountsBridgeTable } from "@/drizzle/schema"
-import { NGOSelector } from "./NGOSelector"
-import { Menu } from "lucide-react"
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-  DropdownMenuSeparator,
-} from "@/components/ui/dropdown-menu"
 import { ClientNavbar } from "./ClientNavbar"
 
 export async function Navbar() {
